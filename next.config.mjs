7a45784c3bf99419620889fa02afe10da2e1f261
Dev25-os <bikeshgupta50041@gmail.com>
2024-06-28 13:52:17 +0530

    removed unused variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    TMDB_API_KEY: process.env.TMDB_API_KEY,
-    BASE_URL: process.env.BASE_URL,
-  },
+ 
 };
 
 export default nextConfig;
